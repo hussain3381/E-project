e21d1a56958cc4db21e90ddb015a0d3eaf48ae38
hussain3381 <hussainsaud053@gmail.com>
2026-05-13 02:12:34 +0500

    improved navbar

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById('menuBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    });
